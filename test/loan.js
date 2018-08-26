71cbc4d03afc0b071992ada40031b30f201582d4
Ryan Riley <ryan.b.riley@colorado.edu>
2018-08-26 12:11:03 -0600

    commenting automated tests

--- test/loan.js
+++ test/loan.js
@@ -1,7 +1,14 @@
 var Loan = artifacts.require("./Loan.sol");
 
 contract('Loan', function(accounts) {
-
+  /*
+   * The following tests the ability of a user to apply for a loan. 
+   * It calls the contract's apply function with a user account
+   * and checks to see that the loanCount has incremented.
+   * This is important because it is a basic function of the 
+   * dApp and without it, there would be no loans on which 
+   * to lend 
+   */
   it("User should be able to apply for a loan", function() {
     return Loan.deployed().then(function(instance) {
       loanInstance = instance;
@@ -13,6 +20,15 @@ contract('Loan', function(accounts) {
       assert.equal(loanCount, 1, "The user was unable to apply for a loan");
     });
   });
+  /* 
+   * The following tests the ability of a user to lend 
+   * to a loan. It creates a loan by having an account
+   * apply for one, then calls the lend function from
+   * another account and checks to see that the loan
+   * has been updated with the lender information.
+   * This is important because without the ability to lend
+   * the dApp would not be useful.
+   */ 
   it("User should be able to lend Ether to loan", function() {
     return Loan.deployed().then(function(instance) {
       loanInstance = instance;
