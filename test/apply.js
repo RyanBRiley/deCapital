0bc57122b45942111e026038ca3c0c522f198ecf
Ryan Riley <ryan.b.riley@colorado.edu>
2018-08-26 11:18:32 -0600

    adding automated test for loan application

---
+++ test/apply.js
@@ -0,0 +1,17 @@
+var Loan = artifacts.require("./Loan.sol");
+
+contract('Loan', function(accounts) {
+
+  it("User should be able to apply for a loan", function() {
+    return Loan.deployed().then(function(instance) {
+      loanInstance = instance;
+
+      return loanInstance.apply(20, {from: accounts[0]});
+    }).then(function() {
+      return loanInstance.loanCount.call();
+    }).then(function(loanCount) {
+      assert.equal(loanCount, 1, "The user was unable to apply for a loan");
+    });
+  });
+
+});
