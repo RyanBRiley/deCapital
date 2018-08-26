82ef965f8bcde28de1e85167ab826bcbbd6ce7fe
Ryan Riley <ryan.b.riley@colorado.edu>
2018-08-26 12:00:39 -0600

    adding test for lending

--- test/loan.js
+++ test/loan.js
@@ -13,4 +13,17 @@ contract('Loan', function(accounts) {
       assert.equal(loanCount, 1, "The user was unable to apply for a loan");
     });
   });
+  it("User should be able to lend Ether to loan", function() {
+    return Loan.deployed().then(function(instance) {
+      loanInstance = instance;
+
+      return loanInstance.apply(20, {from: accounts[0]});
+    }).then(function() {
+      return loanInstance.lend(0, {from: accounts[1], value: web3.toWei('20', 'ether')});
+    }).then(function() {
+      return loanInstance.loans(0);
+    }).then(function(loanData) {
+      assert.equal(loanData.toString().split(',')[4], accounts[1], "The user was unable to lend on a loan");
+    });
+  });
 });
