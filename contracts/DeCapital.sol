51f5cda74f309cc96525e73a38f9a0d45c99bc0f
Ryan Riley <ryan.b.riley@colorado.edu>
2018-08-27 16:09:14 -0600

    refactoring smart contract, adding more SafeMath

--- contracts/DeCapital.sol
+++ contracts/DeCapital.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.4.7;
 
+import "./lib/zeppelin/SafeMath.sol";
+/*
+DeCapital contract - 
+a smart contract for lending and borrowing
+loans on the Ethereum Blockchain
+*/
 
-import "./zeppelin/lifecycle/Killable.sol";
-import "./zeppelin/ownership/Ownable.sol";
-
-contract DeCapital is Ownable, Killable{
+contract DeCapital {
+    
+    //The following line integrates Open Zeppelin's SafeMath library
+    using SafeMath for uint;
+    
+    address public owner;
     uint public loanCount;
     mapping (uint => Loan) public loans;
     
@@ -26,19 +34,41 @@ contract DeCapital is Ownable, Killable{
     
     constructor() public {
         loanCount = 0;
+        owner = msg.sender;
     }
     
-    /*  
-        Function allows users to ask for a specific amount in eth.
-        Amount is converted to wei 
+    
+    /*
+    onlyOwner modifier ensures that the 
+    subsequent function is executed solely by
+    the owner of the contract
+    */
+    modifier onlyOwner() {
+        if(msg.sender == owner) {
+            _;
+        }
+    }
+    
+    /*
+    killContract function checks that the 
+    caller is the owner and then executes
+    self destruct on the contract
+    */  
+    function killContract() public onlyOwner {
+        selfdestruct(owner);
+    }
+    
+    /*
+    Function allows users to ask for a specific amount in eth.
+    Amount is converted to wei 
     */
     function apply(uint _amount) public {
         uint _rate = calculateInterestRate();
         emit Applied(loanCount);
         loans[loanCount] = Loan({loanId: loanCount, 
-                                 amount: _amount * 10**18, 
+                                 amount: _amount.mul(10**18), 
                                  rate: _rate,
-                                 balance: (_amount * 10**18) + (_amount * _rate * 10**16),
+                                 balance: (_amount.mul(10**18)).add((_amount.mul(_rate.mul(10**16)))),
                                  state: State.Requested,
                                  borrower: msg.sender, 
                                  lender: 0});
@@ -46,7 +76,8 @@ contract DeCapital is Ownable, Killable{
     }
     
     /*  
-        Function allows users to lend to a borrower 
+    Function allows users to lend to a borrower 
+    Given the loan_id, it transfers the funds
     */
     
     function lend(uint _loanId) 
@@ -61,8 +92,8 @@ contract DeCapital is Ownable, Killable{
     }
     
     /*  
-        Function allows users to make a payment on 
-        an existing loan and calculate if it is paid off in full
+    Function allows users to make a payment on 
+    an existing loan and calculate if it is paid off in full
     */
     function makePayment(uint _loanId)
         public
@@ -71,26 +102,27 @@ contract DeCapital is Ownable, Killable{
         require(loans[_loanId].state == State.Disbursed, "Loan must be in repayment");
         emit PaymentMade(_loanId);
         if (loans[_loanId].balance > msg.value) {
-            loans[_loanId].balance = loans[_loanId].balance - msg.value;
+            loans[_loanId].balance = loans[_loanId].balance.sub(msg.value);
             loans[_loanId].lender.transfer(msg.value);
         } else {
             loans[_loanId].state = State.Settled;
             loans[_loanId].lender.transfer(loans[_loanId].balance);
-            loans[_loanId].borrower.transfer(msg.value - loans[_loanId].balance);
+            loans[_loanId].borrower.transfer(msg.value.sub(loans[_loanId].balance));
             loans[_loanId].balance = 0;
         }
            
     }
     
     /*  
-        This function returns the interest rate for the loan.
-        This is a stub until there are smarter ways of 
-        determining credit-worthiness (ie. an oracle).
-        In the mean-time, assign a random interest rate between
-        3 and 8 percent
+    This function returns the interest rate for the loan.
+    This is a stub until there are smarter ways of 
+    determining credit-worthiness (ie. an oracle).
+    In the mean-time, assign a pseudo random interest rate between
+    3 and 8 percent. Note that block.timestamp should not be used
+    for security dependent features. 
     */
     function calculateInterestRate() private view returns (uint8) {
-        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 5 + 3);
+        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))).mod(5).add(3));
     }
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,8 @@
-var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
-var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
+var SafeMath = artifacts.require("./lib/zeppelin/SafeMath.sol");
 var Loan = artifacts.require("./DeCapital.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Ownable);
-  deployer.link(Ownable, Killable);
-  deployer.deploy(Killable);
-  deployer.link(Killable, Loan);
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, Loan);
   deployer.deploy(Loan)
 };
